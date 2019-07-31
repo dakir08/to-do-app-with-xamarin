cfe9418391040c38c6cbd4c6c8ad27c50f2f29b8
Yuuki <minhphuongsony@gmail.com>
2019-07-31 16:15:06 +1000

    first commit

---
+++ toDoApp/toDoApp/newToDoPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace toDoApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class newToDoPage : ContentPage
+	{
+		public newToDoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "Event handled !", "Wow");
+        }
+    }
+}
